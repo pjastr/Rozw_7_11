1e8f8b580a1459d72cbca124b46576c5655fa4f6
Piotrek <@>
2017-11-29 14:44:11 +0100

    przeslanie plikow

---
+++ Rozw_7_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Rozw_7_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists("ABC"))
+            {
+                Directory.CreateDirectory("ABC");
+            }
+            
+
+            using (StreamWriter sw = new StreamWriter("ABC\\123.txt"))
+            {
+                sw.Write("s98565");
+            }             
+
+        }
+    }
+}
